9e32a7f98d3be4a0c8d4aa38cfb07ada1929e069
plozovik <plozovikov@gmail.com>
2024-06-29 19:19:20 +0200

    formatting

--- app/ui/tech-stack.tsx
+++ app/ui/tech-stack.tsx
@@ -9,12 +9,8 @@ export default async function TechStack() {
 
   return (
     <div className="flex w-full flex-col md:col-span-4">
-      <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
-        Ops
-      </h2>
+      <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>Ops</h2>
       <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
-        {/* NOTE: comment in this code when you get to this point in the course */}
-
         <div className="bg-white px-6">
           {latestInvoices.map((invoice, i) => {
             return (
