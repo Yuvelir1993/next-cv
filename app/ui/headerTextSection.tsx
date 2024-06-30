3ec4ab3a34f3456a63f70185bd2617357a356aea
plozovik <plozovikov@gmail.com>
2024-06-30 11:16:55 +0200

    updated header

--- app/ui/headerTextSection.tsx
+++ app/ui/headerTextSection.tsx
@@ -4,9 +4,9 @@ import React from 'react';
 const TextSection: React.FC = () => {
   return (
     <section className="mt-6 flex flex-col items-center justify-center text-center">
-      <h2 className="mb-2 text-2xl font-bold">DevOps Engineer</h2>
+      <h2 className="mb-2 text-2xl font-bold">DevOps & Platform Engineer</h2>
       <p className="max-w-2xl text-lg text-gray-700">
-        I am a DevOps Engineer with extensive experience in automating and
+        I am a DevOps and Platform Engineer with extensive experience in automating and
         optimizing mission-critical deployments in the AWS cloud environment.
         Proficient in containerization and orchestration technologies like
         Docker and Kubernetes, and skilled in Infrastructure as Code with tools
